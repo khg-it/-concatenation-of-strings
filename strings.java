954bdea8644d4a9caea7b2497aad089965ac886a
khg-it <120780238+khg-it@users.noreply.github.com>
2023-02-09 06:13:04 -0800

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,10 @@
+public class strings{
+    public static void main(String args[])
+    {
+        String firname="khushi";
+        String secname="Saraswat";
+        String name=firname+""+secname;
+        System.out.println(name);
+
+    }
+}
